65b21b35d212136f44e0b2f2ad7954fa992c921d
arno <tetranoir@gmail.com>
2021-01-18 18:54:13 -0800

    Fixed jasmine test

--- test/zerial.test.ts
+++ test/zerial.test.ts
@@ -1,14 +1,14 @@
 import 'jasmine';
 
-import {G, I, Z} from '../src/zerial';
+import {I, Z} from '../src/zerial';
 
 // Test 1
-class A extends I(class _ extends G {
-  a = Z.string;
-  b = Z.oneOf(Z.number, Z.boolean);
-  c = Z.literal('ff');
-  d = Z.o(Z.literal('z'));
-  e = Z.literal(true);
+class A extends I({
+  a: Z.string,
+  b: Z.oneOf(Z.number, Z.boolean),
+  c: Z.literal('ff'),
+  d: Z.o(Z.literal('z')),
+  e: Z.literal(true),
 }) { }
 
 describe('Zerial', () => {
